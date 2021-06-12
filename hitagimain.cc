1a61d268ca6a4de8cf16e020a9806d6cd628b62b
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-06-12 10:33:37 -0700

    Do not use std::string for now
    I cannot figure out why the processor is faulting. I'll have to look into it further at some point

--- hitagimain.cc
+++ hitagimain.cc
@@ -7,7 +7,7 @@
 
 extern "C" int doommain (int argc, char** argv) ;
 uint64_t delay(uint64_t count) {
-    volatile uint64_t value = 0;
+    uint64_t value = 0;
     for (uint64_t i = 0; i < count; ++i) {
         ++value;
     }
@@ -17,7 +17,6 @@ int main() {
     BuiltinLED theLED;
     BuiltinTFTDisplay theDisplay;
     BuiltinConsole theConsole;
-    std::string msg2("are very tasty!");
     theConsole.write('d');
     theConsole.write('o');
     theConsole.write('n');
@@ -25,7 +24,7 @@ int main() {
     theConsole.write('t');
     theConsole.write('s');
     theConsole.write(' ');
-    theConsole.writeLine(msg2);
+    theConsole.writeLine("are very tasty!");
     theDisplay.clearScreen();
     volatile uint64_t count = 0;
     volatile uint16_t prevColor = 0xFFFF;
