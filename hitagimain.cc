4251b410c5c3af415c7303177c9fb21f05f5bb36
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-06-20 11:00:12 -0700

    Actually call the function to setup environment variables

--- hitagimain.cc
+++ hitagimain.cc
@@ -27,6 +27,9 @@ int main() {
     theChipset.writeLine("HITAGIMON");
     printf("Built on %s at %s\n", __DATE__, __TIME__);
     printf("--------------------------------------------\n\n\n\n");
+    printf("Setting up Environment Variables....{\n");
+    setupEnvironmentVariables();
+    printf("}...Done\n");
     printf("STARTING UP DOOM!!!\n");
     theDisplay.clearScreen();
     theChipset.toggleLED();
