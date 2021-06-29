d7c05e69a21f6fb60f25fc8da056a05b866c6a6c
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-06-29 15:54:37 -0700

    read

--- brk.cc
+++ brk.cc
@@ -33,54 +33,6 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 #include "IORoutines.h"
 #include "IODevice.h"
 
-namespace
-{
-    int
-    sys_write(int fd, const void *buf, size_t sz, int &nwrite) {
-        nwrite = 0;
-        if (fd >= 3) {
-            if (fd < (getSDCardInterface().getMaximumNumberOfOpenFiles() + 3)) {
-                nwrite = getSDCardInterface().writeFile(fd - 3, buf, sz);
-                return 0;
-            } else {
-                return EBADF;
-            }
-        } else {
-            // builtin files
-            switch (fd) {
-                case STDOUT_FILENO:
-                case STDERR_FILENO:
-                    nwrite = getBasicChipsetInterface().write(reinterpret_cast<char *>(const_cast<void *>(buf)), sz);
-                    break;
-                default:
-                    return EBADF;
-            }
-            return 0;
-        }
-    }
-    int
-    sys_read(int fd, void *buf, size_t sz, int &nread) {
-        //char* theBuf = reinterpret_cast<char*>(buf);
-        nread = 0;
-        if (fd >= 3) {
-            if (fd < (getSDCardInterface().getMaximumNumberOfOpenFiles() + 3)) {
-                nread = getSDCardInterface().readFile(fd - 3, buf, sz);
-                return 0;
-            } else {
-                return EBADF;
-            }
-        } else {
-            // builtin files
-            switch (fd) {
-                case STDIN_FILENO:
-                    nread = getBasicChipsetInterface().read(reinterpret_cast<char *>(buf), sz);
-                    break;
-                default:
-                    return EBADF;
-            }
-        }
-    }
-}
 static char* heapEnd = 0;
 
 extern "C"
@@ -128,19 +80,6 @@ int getpid () {
     return -1;
 }
 
-extern "C"
-int
-read (int fd, void* buf, size_t sz) {
-    //printf("read(%d, 0x%x, %ld)\n", fd, buf, sz);
-    int nread = 0;
-    int r = sys_read (fd, buf, sz, nread);
-    if (r != 0)
-    {
-        errno = r;
-        return -1;
-    }
-    return nread;
-}
 
 extern "C"
 int
