40138cb94d1611cb2602ef686413ce8c0a0cf29c
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-08-15 19:00:16 -0700

    Implement the binary manipulator operations

--- clips/MonitorExtensions.cc
+++ clips/MonitorExtensions.cc
@@ -6,23 +6,49 @@
 #include "clips.h"
 /// @todo fix this
 #include "../chipset/ChipsetInteract.h"
-extern "C" void ExamineByte(Environment*, UDFContext*, UDFValue*);
-extern "C" void ExamineShort(Environment*, UDFContext*, UDFValue*);
-extern "C" void ExamineWord(Environment*, UDFContext*, UDFValue*);
-extern "C" void ExamineLongWord(Environment*, UDFContext*, UDFValue*);
+
+#define X(title) void title (Environment*, UDFContext*, UDFValue*)
+X(ExamineByte);
+X(ExamineShort);
+X(ExamineWord);
+X(ExamineLongWord);
+X(ShiftLeft);
+X(ShiftRight);
+X(BinaryAnd);
+X(BinaryOr);
+X(BinaryNot);
+X(BinaryNor);
+X(BinaryNand);
+X(BinaryXor);
+X(BinaryAndNot);
+X(BinaryNotAnd);
+X(BinaryOrNot);
+X(BinaryNotOr);
+#undef X
 
 extern "C"
 void
 InstallMonitorExtensions(Environment* env) {
-    AddUDF(env, "examine-byte", "ld", 1, 1, "ld", ExamineByte, "ExamineByte",NULL);
-    AddUDF(env, "examine-short", "ld", 1, 1, "ld", ExamineShort, "ExamineShort",NULL);
-    AddUDF(env, "examine-word", "ld", 1, 1, "ld", ExamineWord, "ExamineWord",NULL);
-    AddUDF(env, "examine-long-word", "ld", 1, 1, "ld", ExamineLongWord, "ExamineLongWord",NULL);
+    AddUDF(env, "examine-byte", "l", 1, 1, "l", ExamineByte, "ExamineByte",NULL);
+    AddUDF(env, "examine-short", "l", 1, 1, "l", ExamineShort, "ExamineShort",NULL);
+    AddUDF(env, "examine-word", "l", 1, 1, "l", ExamineWord, "ExamineWord",NULL);
+    AddUDF(env, "examine-long-word", "l", 1, 1, "l", ExamineLongWord, "ExamineLongWord",NULL);
+    AddUDF(env, "shift-left", "l", 2, 2, "l", ShiftLeft, "ShiftLeft", NULL);
+    AddUDF(env, "shift-right", "l", 2, 2, "l", ShiftRight, "ShiftRight", NULL);
+    AddUDF(env, "binary-and", "l", 2, 2, "l", BinaryAnd, "BinaryAnd", NULL);
+    AddUDF(env, "binary-or", "l", 2, 2, "l", BinaryOr, "BinaryOr", NULL);
+    AddUDF(env, "binary-xor", "l", 2, 2, "l", BinaryXor, "BinaryXor", NULL);
+    AddUDF(env, "binary-nor", "l", 2, 2, "l", BinaryNor, "BinaryNor", NULL);
+    AddUDF(env, "binary-nand", "l", 2, 2, "l", BinaryNand, "BinaryNand", NULL);
+    AddUDF(env, "binary-and-not", "l", 2, 2, "l", BinaryAndNot, "BinaryAndNot", NULL);
+    AddUDF(env, "binary-not-and", "l", 2, 2, "l", BinaryNotAnd, "BinaryNotAnd", NULL);
+    AddUDF(env, "binary-not", "l", 1, 1, "l", BinaryNot, "BinaryNot", NULL);
+    AddUDF(env, "binary-or-not", "l", 2, 2, "l", BinaryOrNot, "BinaryOrNot", NULL);
+    AddUDF(env, "binary-not-or", "l", 2, 2, "l", BinaryNotOr, "BinaryNotOr", NULL);
 }
+#define DefClipsFunction(name) void name (Environment* theEnv, UDFContext* context, UDFValue* retVal)
 
-extern "C"
-void
-ExamineByte(Environment* theEnv, UDFContext* context, UDFValue* retVal) {
+DefClipsFunction(ExamineByte) {
     if (! UDFNthArgument(context,1,NUMBER_BITS,retVal)) {
         return;
     }
@@ -30,9 +56,8 @@ ExamineByte(Environment* theEnv, UDFContext* context, UDFValue* retVal) {
         retVal->integerValue = CreateInteger(theEnv, memory<uint8_t>(static_cast<uint32_t>(retVal->integerValue->contents)));
     }
 }
-extern "C"
-void
-ExamineShort(Environment* theEnv, UDFContext* context, UDFValue* retVal) {
+
+DefClipsFunction(ExamineShort) {
     if (! UDFNthArgument(context,1,NUMBER_BITS,retVal)) {
         return;
     }
@@ -40,9 +65,8 @@ ExamineShort(Environment* theEnv, UDFContext* context, UDFValue* retVal) {
         retVal->integerValue = CreateInteger(theEnv, memory<uint16_t>(static_cast<uint32_t>(retVal->integerValue->contents)));
     }
 }
-extern "C"
-void
-ExamineWord(Environment* theEnv, UDFContext* context, UDFValue* retVal) {
+
+DefClipsFunction(ExamineWord) {
     if (! UDFNthArgument(context,1,NUMBER_BITS,retVal)) {
         return;
     }
@@ -50,9 +74,8 @@ ExamineWord(Environment* theEnv, UDFContext* context, UDFValue* retVal) {
         retVal->integerValue = CreateInteger(theEnv, memory<uint32_t>(static_cast<uint32_t>(retVal->integerValue->contents)));
     }
 }
-extern "C"
-void
-ExamineLongWord(Environment* theEnv, UDFContext* context, UDFValue* retVal) {
+
+DefClipsFunction(ExamineLongWord) {
     if (! UDFNthArgument(context,1,NUMBER_BITS,retVal)) {
         return;
     }
@@ -61,3 +84,150 @@ ExamineLongWord(Environment* theEnv, UDFContext* context, UDFValue* retVal) {
         retVal->integerValue = CreateInteger(theEnv, static_cast<int64_t>(memory<uint64_t>(static_cast<uint32_t>(retVal->integerValue->contents))));
     }
 }
+
+DefClipsFunction(ShiftLeft) {
+    UDFValue first, second;
+    if (! UDFNthArgument(context,1,NUMBER_BITS,&first)) {
+        return;
+    }
+    uint64_t base = CVCoerceToInteger(&first);
+    if (!UDFNthArgument(context, 2, NUMBER_BITS, &second)) {
+        return;
+    }
+    uint64_t shiftAmount = CVCoerceToInteger(&second);
+    retVal->integerValue = CreateInteger(theEnv, static_cast<int64_t>(base << shiftAmount));
+}
+
+DefClipsFunction(ShiftRight) {
+    UDFValue first, second;
+    if (! UDFNthArgument(context,1,NUMBER_BITS,&first)) {
+        return;
+    }
+    uint64_t base = CVCoerceToInteger(&first);
+    if (!UDFNthArgument(context, 2, NUMBER_BITS, &second)) {
+        return;
+    }
+    uint64_t shiftAmount = CVCoerceToInteger(&second);
+    retVal->integerValue = CreateInteger(theEnv, static_cast<int64_t>(base >> shiftAmount));
+}
+
+DefClipsFunction(BinaryNor) {
+    UDFValue first, second;
+    if (! UDFNthArgument(context,1,NUMBER_BITS,&first)) {
+        return;
+    }
+    uint64_t a = CVCoerceToInteger(&first);
+    if (!UDFNthArgument(context, 2, NUMBER_BITS, &second)) {
+        return;
+    }
+    uint64_t b = CVCoerceToInteger(&second);
+    retVal->integerValue = CreateInteger(theEnv, static_cast<int64_t>((~a) & (~b)));
+}
+
+DefClipsFunction(BinaryNand) {
+    UDFValue first, second;
+    if (! UDFNthArgument(context,1,NUMBER_BITS,&first)) {
+        return;
+    }
+    uint64_t a = CVCoerceToInteger(&first);
+    if (!UDFNthArgument(context, 2, NUMBER_BITS, &second)) {
+        return;
+    }
+    uint64_t b = CVCoerceToInteger(&second);
+    retVal->integerValue = CreateInteger(theEnv, static_cast<int64_t>((~a) | (~b)));
+}
+DefClipsFunction(BinaryAnd) {
+    UDFValue first, second;
+    if (! UDFNthArgument(context,1,NUMBER_BITS,&first)) {
+        return;
+    }
+    uint64_t a = CVCoerceToInteger(&first);
+    if (!UDFNthArgument(context, 2, NUMBER_BITS, &second)) {
+        return;
+    }
+    uint64_t b = CVCoerceToInteger(&second);
+    retVal->integerValue = CreateInteger(theEnv, static_cast<int64_t>((a) & (b)));
+}
+
+DefClipsFunction(BinaryOr) {
+    UDFValue first, second;
+    if (! UDFNthArgument(context,1,NUMBER_BITS,&first)) {
+        return;
+    }
+    uint64_t a = CVCoerceToInteger(&first);
+    if (!UDFNthArgument(context, 2, NUMBER_BITS, &second)) {
+        return;
+    }
+    uint64_t b = CVCoerceToInteger(&second);
+    retVal->integerValue = CreateInteger(theEnv, static_cast<int64_t>((a) | (b)));
+}
+DefClipsFunction(BinaryXor) {
+    UDFValue first, second;
+    if (! UDFNthArgument(context,1,NUMBER_BITS,&first)) {
+        return;
+    }
+    uint64_t a = CVCoerceToInteger(&first);
+    if (!UDFNthArgument(context, 2, NUMBER_BITS, &second)) {
+        return;
+    }
+    uint64_t b = CVCoerceToInteger(&second);
+    retVal->integerValue = CreateInteger(theEnv, static_cast<int64_t>((a) ^ (b)));
+}
+DefClipsFunction(BinaryAndNot) {
+    UDFValue first, second;
+    if (! UDFNthArgument(context,1,NUMBER_BITS,&first)) {
+        return;
+    }
+    uint64_t a = CVCoerceToInteger(&first);
+    if (!UDFNthArgument(context, 2, NUMBER_BITS, &second)) {
+        return;
+    }
+    uint64_t b = CVCoerceToInteger(&second);
+    retVal->integerValue = CreateInteger(theEnv, static_cast<int64_t>((a) & (~b)));
+}
+DefClipsFunction(BinaryNotAnd) {
+    UDFValue first, second;
+    if (! UDFNthArgument(context,1,NUMBER_BITS,&first)) {
+        return;
+    }
+    uint64_t a = CVCoerceToInteger(&first);
+    if (!UDFNthArgument(context, 2, NUMBER_BITS, &second)) {
+        return;
+    }
+    uint64_t b = CVCoerceToInteger(&second);
+    retVal->integerValue = CreateInteger(theEnv, static_cast<int64_t>((~a) & (b)));
+}
+
+DefClipsFunction(BinaryNot) {
+    if (! UDFNthArgument(context,1,NUMBER_BITS,retVal)) {
+        return;
+    }
+    retVal->integerValue = CreateInteger(theEnv, ~CVCoerceToInteger(retVal));
+}
+
+DefClipsFunction(BinaryOrNot) {
+    UDFValue first, second;
+    if (! UDFNthArgument(context,1,NUMBER_BITS,&first)) {
+        return;
+    }
+    uint64_t a = CVCoerceToInteger(&first);
+    if (!UDFNthArgument(context, 2, NUMBER_BITS, &second)) {
+        return;
+    }
+    uint64_t b = CVCoerceToInteger(&second);
+    retVal->integerValue = CreateInteger(theEnv, static_cast<int64_t>((a) | (~b)));
+}
+DefClipsFunction(BinaryNotOr) {
+    UDFValue first, second;
+    if (! UDFNthArgument(context,1,NUMBER_BITS,&first)) {
+        return;
+    }
+    uint64_t a = CVCoerceToInteger(&first);
+    if (!UDFNthArgument(context, 2, NUMBER_BITS, &second)) {
+        return;
+    }
+    uint64_t b = CVCoerceToInteger(&second);
+    retVal->integerValue = CreateInteger(theEnv, static_cast<int64_t>((~a) | (b)));
+}
+
+#undef DefClipsFunction
