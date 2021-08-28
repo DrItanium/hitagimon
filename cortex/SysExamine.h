41ae482809514efd1ed26e8d0839394448021c83
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-08-28 16:47:32 -0700

    Got the union setup

--- cortex/SysExamine.h
+++ cortex/SysExamine.h
@@ -19,6 +19,28 @@ namespace cortex {
 #else
             uint32_t unused0 : 5;
 #endif
+            uint32_t integerOverflowFlag : 1;
+            uint32_t unused1 : 1;
+            uint32_t integerOverflowMask : 1;
+            uint32_t unused2 : 2;
+            uint32_t noImpreciseFaults : 1;
+#ifdef __i960SB__
+            uint32_t floatingOverflowFlag : 1;
+            uint32_t floatingUnderflowFlag : 1;
+            uint32_t floatingInvalidOpFlag : 1;
+            uint32_t floatingZeroDivideFlag : 1;
+            uint32_t floatingInexactFlag : 1;
+            uint32_t unused3 : 3;
+            uint32_t floatingOverflowMask : 1;
+            uint32_t floatingUnderflowMask : 1;
+            uint32_t floatingInvalidOpMask : 1;
+            uint32_t floatingZeroDivideMask : 1;
+            uint32_t floatingInexactMask : 1;
+            uint32_t floatingPointNormalizingMode : 1;
+            uint32_t floatingPointRoundingControl : 2;
+#else
+            uint32_t unused3 : 16;
+#endif
         };
     };
 }
