bbea0d0400bdac41d6d16ba3b7859af158cb7739
Joshua Scoggins <Theoretically.x64@gmail.com>
2021-12-16 21:16:47 -0800

    Display we are starting up clips

--- hitagimain.cc
+++ hitagimain.cc
@@ -62,6 +62,7 @@ int main() {
     printf("OK? theValue64 = 0x%x%x but expecting 0x9876'5432'10AB'CDEF\n", static_cast<unsigned>(theValue64), static_cast<unsigned>(theValue64 >> 32));
     setISR0Function(isr0Test);
     displayIOMemoryMap();
+    printf("Starting up CLIPS!\n");
     clipsMain(0, args);
     return 0;
 }
