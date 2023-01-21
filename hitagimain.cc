482b5f836b8829cf25fcb602ceb7aa9972c1ca9a
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-01-21 09:57:24 -0800

    Do a print every 1 second

--- hitagimain.cc
+++ hitagimain.cc
@@ -56,7 +56,7 @@ int main() noexcept {
         printf("Counter: %x%x\n", static_cast<unsigned int>(start), static_cast<unsigned int>(start >> 32));
         do {
             uint64_t difference = cortex::getSystemCounter() - start;
-            if (difference >= 1000) {
+            if (difference >= 10) {
                 break;
             }
         } while (true);
