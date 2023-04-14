fefb2f1498dc702f4b03342cf06f10c78e81f004
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-04-14 14:15:47 -0700

    Implemented a basic version of __builtin_mul_overflow

--- cortex/ModernGCC.cc
+++ cortex/ModernGCC.cc
@@ -5,24 +5,31 @@
 #include "ModernGCC.h"
 #ifndef __BUILTIN_MUL_OVERFLOW
 #include "SysExamine.h"
+
+inline uint32_t setArithmeticControls(uint32_t mask, uint32_t src) {
+    uint32_t result = 0;
+    asm volatile ("modac %2, %1, %0" : "=r" (result) :  "r" (src) , "r" (mask));
+    return result;
+}
+inline uint32_t setArithmeticControls(uint32_t value) {
+    asm volatile ("modac %0, %0, %0" : "=&r" (value));
+    return value;
+}
+inline uint32_t getArithmeticControls() {
+    uint32_t result = 0;
+    asm volatile ("modac 0, 0, %0" : "=r" (result));
+    return result;
+}
 extern "C"
 bool
 __builtin_mul_overflow(size_t a, size_t b, size_t* res) {
-    register uint32_t save asm("r14");
-    cortex::ArithmeticControls intermediate;
-    do {
-        asm volatile ("modac 0, 0, %0": "=r" (save));
-        intermediate.raw = save;
-        intermediate.integerOverflowMask = 1;
-    } while (false);
-    SetArithmeticControls(intermediate);
+    uint32_t save = setArithmeticControls(0x500, 0x400);
     do {
         *res = a * b;
     } while (false);
-    GetArithmeticControls(intermediate);
-    bool result = intermediate.integerOverflowFlag;
-    asm volatile ("modac %0, %0, %0": "=&r" (save));
-    return result;
+    uint32_t result = getArithmeticControls();
+    setArithmeticControls(save);
+    return (result & 0x100);
 }
 
 #endif
