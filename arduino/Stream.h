fe6bceb9a1e13a74b835bffe8b3a0f455f423a55
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-04-15 11:07:45 -0700

    Stream header done

--- arduino/Stream.h
+++ arduino/Stream.h
@@ -35,11 +35,23 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 
 enum LookaheadMode {
-    SKIP_ALL, // All invalid characters are ignored
-    SKIP_NONE, // Nothing is skipped, and the stream is not touched unless the first waiting character is valid
-    SKIP_WHITESPACE,  // Only tabs, spaces, line feeds & carriage returns are skipped
+    /**
+     * @brief All invalid characters are ignored
+     */
+    SKIP_ALL,
+    /**
+     * @brief Nothing is skipped, and the stream is not touched unless the first waiting character is valid
+     */
+    SKIP_NONE,
+    /**
+     * @brief Only tabs, spaces, line feeds, and carriage returns are skipped
+     */
+    SKIP_WHITESPACE,
 };
-#define NO_IGNORE_CHAR '\x01' // a char not found in a valid ascii numeric field
+/**
+ * @brief A character not found in a valid ascii numeric field
+ */
+#define NO_IGNORE_CHAR '\x01'
 
 class Stream : public Print {
 
@@ -49,12 +61,91 @@ public:
     virtual int peek() noexcept = 0;
     Stream() : timeout_(1000), startMillis_(0) { }
     virtual ~Stream() { }
+    // parsing methods
     void setTimeout(unsigned long timeout) noexcept;
     constexpr unsigned long getTimeout() const noexcept { return timeout_; }
     bool find(char* target) noexcept;
     bool find(uint8_t* target) noexcept { return find ((char*)target); }
     bool find(char* target, size_t length) noexcept;
     bool find(uint8_t* target, size_t length) noexcept { return find((char*)target, length); }
+
+    bool find (char target) { return find (&target, 1); }
+
+    bool findUntil(char* target, char* terminator);
+    bool findUntil(uint8_t * target, char* terminator) { return findUntil((char* )target, terminator); }
+
+    bool findUntil(char* target, size_t targetLen, char* terminator, size_t termLen);
+    bool findUntil(uint8_t * target, size_t targetLen, char* terminator, size_t termLen) { return findUntil((char* )target, targetLen, terminator, termLen); }
+
+    /**
+     * @brief Returns the first valid (long) integer value from the current position.
+     * lookahead determines how parseInt lookahead in the stream.
+     * @param lookahead What kind of lookahead to use
+     * @param ignore the characters to be skipped in the stream
+     * @return the first valid (long) integer
+     */
+    long parseInt(LookaheadMode lookahead = SKIP_ALL, char ignore = NO_IGNORE_CHAR) noexcept;
+
+    /**
+     * @brief Float version of parseInt
+     * @param lookahead how to perform the character lookahead
+     * @param ignore The character to ignore outright
+     * @return The first valid float
+     */
+    float parseFloat(LookaheadMode lookahead = SKIP_ALL, char ignore = NO_IGNORE_CHAR) noexcept;
+
+    /**
+     * @brief Read chars from stream into buffer until length is hit or a timeout is reached
+     * @param buffer  the buffer to read into
+     * @param length the number of characters to read max
+     * @return The actual number of characters read (possible to be different from length)
+     */
+    size_t readBytes(char* buffer, size_t length);
+    size_t readBytes(uint8_t* buffer, size_t length) { return readBytes((char*)buffer, length); }
+
+    /**
+     * @brief read bytes into a stream until length has been hit, a timeout has been reached, or the provided terminator character is found
+     * @param terminator The character to terminate reading if encountered
+     * @param buffer The buffer to read into
+     * @param length The number of characters to read max
+     * @return The actual number of characters read into the buffer
+     */
+    size_t readBytesUntil(char terminator, char* buffer, size_t length);
+    size_t readBytesUntil(char terminator, uint8_t* buffer, size_t length) { return readBytesUntil(terminator, (char*)buffer, length); }
+
+    String readString();
+    String readStringUntil(char terminator);
+protected:
+    // internal methods for keeping the public API simple
+    long parseInt(char ignore) { return parseInt(SKIP_ALL, ignore); }
+    float parseFloat(char ignore) { return parseFloat(SKIP_ALL, ignore); }
+
+    /**
+     * @brief Allows one to search for an arbitrary number of strings, this is a single search criteria
+     */
+    struct MultiTarget {
+        /**
+         * @brief The string being searched for
+         */
+        const char* str;
+        /**
+         * @brief Length of the string being searched for
+         */
+        size_t len;
+        /**
+         * @brief Index used by the search routine
+         */
+        size_t index;
+    };
+
+    /**
+     * @brief Search for an arbitrary number of strings
+     * @param targets The targets to search through
+     * @param tCount the number of targets
+     * @return The index of the target that is found first or -1 if timeout occurs
+     */
+    int findMulti(MultiTarget* targets, int tCount);
+
 protected:
     int timedRead() noexcept;
     int timedPeek() noexcept;
