c6dde2943354c82b785e7fcf322168c1d2c6368e
Joshua Scoggins <Theoretically.x64@gmail.com>
2023-05-13 12:37:08 -0700

    Drop the opcode struct
    No longer needed

--- cortex/ChipsetInteract.h
+++ cortex/ChipsetInteract.h
@@ -36,45 +36,5 @@ namespace cortex
     template<typename T>
     inline volatile T &
     memory(const uint32_t address) { return *reinterpret_cast<T *>(address); }
-
-
-    /**
-     * @brief 28-bit opcode that is converted to an address and sent to the microcontroller to be interepreted as an action
-     */
-    union Opcode {
-        Opcode(uint32_t whole = 0) : full_(whole) { }
-        Opcode(uint8_t group, uint8_t function, uint16_t subminor) : subminor(subminor), function(function), group(group) { }
-        Opcode(uint8_t group, uint8_t device, uint8_t operation, uint8_t subminor) : lowest(subminor), deviceOperation(operation),
-        targetDevice(device), deviceGroup(group) { }
-        uint32_t full_ : 28;
-        struct {
-            uint32_t subminor : 16;
-            uint32_t function : 8;
-            uint32_t group : 4;
-        };
-        struct {
-            uint32_t lowest : 8;
-            uint32_t deviceOperation : 8;
-            uint32_t targetDevice : 8;
-            uint32_t deviceGroup : 4;
-        };
-
-        uint32_t makeFullAddress() const {
-            return 0xF0000000 | full_;
-        }
-        template<typename T>
-        inline volatile T& memory() {
-            return *reinterpret_cast<T*>(makeFullAddress());
-        }
-
-        inline void write8(uint8_t value) { memory<uint8_t>() = value; }
-        inline void write16(uint16_t value) { memory<uint16_t>() = value; }
-        inline void write32(uint32_t value) { memory<uint32_t>() = value; }
-        inline void write64(uint64_t value) { memory<uint64_t>() = value; }
-        inline uint8_t read8() { return memory<uint8_t>(); }
-        inline uint16_t read16() { return memory<uint16_t>(); }
-        inline uint32_t read32() { return memory<uint32_t>(); }
-        inline uint64_t read64() { return memory<uint64_t>(); }
-    };
 }
 #endif //I960SXCHIPSET_PERIPHERALS_H
