4a8ffb1e3e3f52c09918df5ba2c125d09a716737
Joshua Scoggins <Theoretically.x64@gmail.com>
2024-11-16 11:05:29 -0800

    setters for LTR390 device plus device getter

--- cortex/IODevice.h
+++ cortex/IODevice.h
@@ -248,18 +248,20 @@ namespace cortex
             ALS,
             UVS,
         } mode_t;
-        bool newDataAvailable() const noexcept { return _newDataAvailable != 0; }
-        operator bool() const noexcept { return newDataAvailable(); }
-        bool available() const noexcept { return _available != 0; }
-        resolution_t getResolution() const noexcept { return static_cast<resolution_t>(_resolution); }
-        gain_t getGain() const noexcept { return static_cast<gain_t>(_gain); }
-        mode_t getMode() const noexcept { return static_cast<mode_t>(_mode); }
-        bool inALSMode() const noexcept { return getMode() == ALS; }
-        bool inUVSMode() const noexcept { return getMode() == UVS; }
-        uint32_t readUVS() const noexcept { return _uvs; }
-        uint32_t readALS() const noexcept { return _als; }
-
-
+        bool newDataAvailable() volatile const noexcept { return _newDataAvailable != 0; }
+        operator bool() volatile const noexcept { return newDataAvailable(); }
+        bool available() volatile const noexcept { return _available != 0; }
+        resolution_t getResolution() volatile const noexcept { return static_cast<resolution_t>(_resolution); }
+        gain_t getGain() volatile const noexcept { return static_cast<gain_t>(_gain); }
+        mode_t getMode() volatile const noexcept { return static_cast<mode_t>(_mode); }
+        bool inALSMode() volatile const noexcept { return getMode() == ALS; }
+        bool inUVSMode() volatile const noexcept { return getMode() == UVS; }
+        uint32_t readUVS() volatile const noexcept { return _uvs; }
+        uint32_t readALS() volatile const noexcept { return _als; }
+        void setGain(gain_t value) volatile noexcept { _gain = value; }
+        void setMode(mode_t value) volatile noexcept { _mode = value; }
+        void setResolution(resolution_t value) volatile noexcept { _resolution = value; }
+        static volatile LTR390Device& get() noexcept { return memory<LTR390Device>(0xFE000100); }
     } __attribute__((packed));
 }
 #endif //I960SXCHIPSET_IODEVICE_H
