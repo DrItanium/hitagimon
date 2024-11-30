39efb784a970f6adadbaa5c2e81df0d116059048
Joshua Scoggins <Theoretically.x64@gmail.com>
2024-11-29 22:26:01 -0800

    Boot as fast as possible
    Also, do not reconfigure the interrupts after the fact.

--- hitagimain.cc
+++ hitagimain.cc
@@ -60,9 +60,6 @@ namespace microshell {
 void
 init()
 {
-    // first setup the interrupt control registers
-    // it is int0 is lowest byte and so on
-    __builtin_i960_set_interrupt_control_reg(0xFCFDFEFF);
     // setup the chipset basic functions as one of the first things we actually do
     cortex::ChipsetBasicFunctions::begin();
     // then setup the system clock to be 200 hz so that we trigger every 10 ms
@@ -134,37 +131,11 @@ private:
 };
 void
 setup() {
-    std::cout << "HITAGIMON" << std::endl
-              << "Built on " << __DATE__ << " at " << __TIME__ << std::endl
-              << "--------------------------------------------" << std::endl << std::endl << std::endl << std::endl
-              << "NEWLIB Version: " << _NEWLIB_VERSION << std::endl
-              << "Starting counter: 0x" << std::hex << cortex::getSystemCounter() << std::endl;
-    std::cout << "millis: " << std::dec << cortex::ChipsetBasicFunctions::Timer::millis() << " millseconds since chipset boot" << std::endl;
-    std::cout << "micros: " << std::dec << cortex::ChipsetBasicFunctions::Timer::micros() << " microseconds since chipset boot" << std::endl;
-    struct rusage ru;
-    getrusage(RUSAGE_SELF, &ru);
-    std::cout << "rusage.sec: " << std::dec << ru.ru_utime.tv_sec << " sec" << std::endl;
-    std::cout << "rusage.usec: " << std::dec << ru.ru_utime.tv_usec << " usec" << std::endl;
-    if (cortex::ChipsetBasicFunctions::Disk0::available()) {
-        std::cout << "Found disk 0!" << std::endl;
-        std::cout << "Capacity: " << std::dec << cortex::ChipsetBasicFunctions::Disk0::size() << " bytes" << std::endl;
-        std::cout << "Position: " << std::dec << cortex::ChipsetBasicFunctions::Disk0::getPosition() << std::endl;
-    } else {
-        std::cout << "Disk 0 is not installed!" << std::endl;
-    }
-#if 0
-    std::cout << "Boot Words information: " << std::endl;
-    const volatile cortex::BootWords& theWords = cortex::getBootWords();
-    std::cout << "\tSegment Table Start: " << std::hex << theWords.sat << std::endl;
-    std::cout << "\tBoot PRCB Start: " << std::hex << theWords.thePRCB << std::endl;
-    std::cout << "\tFirst Instruction starts at: 0x" << std::hex << reinterpret_cast<uintptr_t>(theWords.firstInstruction) << std::endl;
-    cortex::SegmentTable& segTable = *theWords.sat;
-    std::cout << "Boot Segment Table Information: " << std::endl;
-    printBaseSegmentTable(std::cout, segTable, 12);
-    cortex::SegmentTable& currTable = *cortex::getSystemAddressTable();
-    std::cout << "Current Segment Table Information: " << std::endl;
-    printBaseSegmentTable(std::cout, currTable, 12);
-#endif
+    //std::cout << "HITAGIMON" << std::endl
+    //          << "Built on " << __DATE__ << " at " << __TIME__ << std::endl
+    //          << "--------------------------------------------" << std::endl << std::endl << std::endl << std::endl;
+    //std::cout << "millis: " << std::dec << cortex::ChipsetBasicFunctions::Timer::millis() << " millseconds since chipset boot" << std::endl;
+    //std::cout << "micros: " << std::dec << cortex::ChipsetBasicFunctions::Timer::micros() << " microseconds since chipset boot" << std::endl;
     microshell::setup();
 }
 void loop() {
