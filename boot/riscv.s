f5cc9b45bca7b658953c56f3dc0fe729ea0d8c98
Joshua Scoggins <Theoretically.x64@gmail.com>
2025-03-29 19:28:54 -0700

    Define the opcode map

--- boot/riscv.s
+++ boot/riscv.s
@@ -29,8 +29,8 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # pfp -> i960 previous frame pointer
 # sp ->  i960 stack pointer
 # rip -> i960 return instruction pointer
-# r3 -> 
-# r4 -> 
+# r3 -> instruction contents
+# r4 -> opcode
 # r5 -> 
 # r6 -> 
 # r7 -> 
@@ -62,6 +62,43 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 # we want to actually map the emulator code into the microcontroller itself
 # that way, we can actually keep the lowest 64 megabytes completely free. 
 .include "macros.s"
+# taken from the isa overview
+.set OPCODE_LOAD,      0b0000011
+.set OPCODE_STORE,     0b0100011
+.set OPCODE_MADD,      0b1000011
+.set OPCODE_BRANCH,    0b1100011
+
+.set OPCODE_LOAD_FP,   0b0000111
+.set OPCODE_STORE_FP,  0b0100111
+.set OPCODE_MSUB, 	   0b1000111
+.set OPCODE_JALR,      0b1100111
+
+.set OPCODE_CUSTOM_0,  0b0001011
+.set OPCODE_CUSTOM_1,  0b0101011
+.set OPCODE_NMSUB,     0b1001011
+.set OPCODE_RESERVED,  0b1101011
+
+.set OPCODE_MISC_MEM,  0b0001111
+.set OPCODE_AMO,       0b0101111
+.set OPCODE_NMADD,     0b1001111
+.set OPCODE_JAL,       0b1101111
+
+.set OPCODE_OP_IMM,    0b0010011
+.set OPCODE_OP,        0b0110011
+.set OPCODE_FP,        0b1010011
+.set OPCODE_SYSTEM,    0b1110011
+
+.set OPCODE_AUIPC,     0b0010111
+.set OPCODE_LUI,       0b0110111
+.set OPCODE_OP_V,      0b1010111
+.set OPCODE_OP_VE,     0b1110111
+
+.set OPCODE_OP_IMM_32, 0b0011011
+.set OPCODE_OP_32,     0b0111011
+.set OPCODE_CUSTOM_2,  0b1011011
+.set OPCODE_CUSTOM_3,  0b1111011
+
+
 .text
 .align 6
 .global riscv_emulator_start
@@ -75,7 +112,17 @@ riscv_emulator_start:
 	movq 0, g4
 	movq 0, g8
 	movt 0, g12
-	b riscv_emulator_start
+instruction_decoder_body:
+	ld 0(g13), r3 # load the current instruction
+	mov r3, r4    # make a copy of it
+	extract 0, 7, r4 # opcode extraction
+next_instruction:
+	addo g13, 4, g13 # go to the next instruction
+	b instruction_decoder_body 
+rv32_auipc:
+	bx next_instruction
+rv32_lui:
+	bx next_instruction
 .data
 .align 6
 hart0_gpr_register_file:
