ade966338f798c1dab264e263e81878d01386ca1
Joshua Scoggins <Theoretically.x64@gmail.com>
2026-03-21 17:26:39 -0700

    Initial import of benchmarks.h

---
+++ benchmarks/benchmarks.h
@@ -0,0 +1,41 @@
+/*
+hitagimon
+Copyright (c) 2020-2026, Joshua Scoggins
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+#ifndef HITAGIMON_BENCHMARKS_H__
+#define HITAGIMON_BENCHMARKS_H__
+#include <cstdint>
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * @brief Perform count number of move operations; tests how long it takes to perform the moves
+ * @param count The number of times to do it
+ */
+void ordinalRegisterToRegisterMoveTest(uint32_t count);
+#ifdef __cplusplus
+}
+#endif
+#endif //HITAGIMON_BENCHMARKS_H__
