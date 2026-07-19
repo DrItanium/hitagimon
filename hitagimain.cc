9b7e60923329f75cb887c7608d9d94ab8100776c
Joshua Scoggins <Theoretically.x64@gmail.com>
2026-07-19 15:27:34 -0700

    Simple mandlebrot adaption from the ILI9341 examples

--- hitagimain.cc
+++ hitagimain.cc
@@ -1778,6 +1778,55 @@ namespace microshell {
         doGraphicsTestIno();
         std::cout << "Done!" << std::endl;
     }, nullptr, nullptr, nullptr 
+},
+{ "mandlebrot", 
+    "run the adaption of examples/mandlebrot.ino for this target",
+    nullptr,
+    [](ush_object* self, ush_file_descriptor const* file, int argc, char* argv[]) {
+        static const int16_t bits        = 20;   // Fractional resolution
+        static const int16_t pixelWidth  = GraphicsInterface::width();  // TFT dimensions
+        static const int16_t pixelHeight  = GraphicsInterface::height();  // TFT dimensions
+        static const int16_t iterations  = 128;  // Fractal iteration limit or 'dwell'
+        static float centerReal  = -0.6, // Image center point in complex plane
+                     centerImag  =  0.0,
+                     rangeReal   =  3.0, // Image coverage in complex plane
+                     rangeImag   =  3.0; 
+        int64_t       n, a, b, a2, b2, posReal, posImag;
+        uint32_t      startTime,elapsedTime;
+
+
+        int32_t
+            startReal   = (int64_t)((centerReal - rangeReal * 0.5)   * (float)(1 << bits)),
+                        startImag   = (int64_t)((centerImag + rangeImag * 0.5)   * (float)(1 << bits)),
+                        incReal     = (int64_t)((rangeReal / (float)pixelWidth)  * (float)(1 << bits)),
+                        incImag     = (int64_t)((rangeImag / (float)pixelHeight) * (float)(1 << bits));
+
+        startTime = millis();
+        posImag = startImag;
+        for (int y = 0; y < pixelHeight; y++) {
+            posReal = startReal;
+            for (int x = 0; x < pixelWidth; x++) {
+                a = posReal;
+                b = posImag;
+                for (n = iterations; n > 0 ; n--) {
+                    a2 = (a * a) >> bits;
+                    b2 = (b * b) >> bits;
+                    if ((a2 + b2) >= (4 << bits)) 
+                        break;
+                    b  = posImag + ((a * b) >> (bits - 1));
+                    a  = posReal + a2 - b2;
+                }
+                GraphicsInterface::drawPixel(x, y, (n * 29)<<8 | (n * 67)); // takes 500ms with individual pixel writes
+                posReal += incReal;
+            }
+            posImag -= incImag;
+        }
+        elapsedTime = millis()-startTime;
+        std::cout << "Took " << std::dec << elapsedTime << " ms" << std::endl;
+
+        rangeReal *= 0.95;
+        rangeImag *= 0.95;
+    }, nullptr, nullptr, nullptr 
 }
 
 };
