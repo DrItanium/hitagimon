b3b50464f48c54d5adec89bcfca8488c592bb0fc
Joshua Scoggins <Theoretically.x64@gmail.com>
2026-07-19 19:48:21 -0700

    Start adding support for display updates

--- cortex/IODevice.h
+++ cortex/IODevice.h
@@ -184,6 +184,7 @@ namespace cortex {
                 print(value, style);
                 println();
             }
+            void updateDisplay(int16_t x, int16_t y, int16_t w, int16_t h);
         }
         namespace Random {
             uint32_t getHardwareRandomNumber() noexcept;
@@ -226,5 +227,5 @@ namespace cortex {
         FilesystemInterfaceErrorCodes postRequest(RequestStructure request);
     } // end namespace SDCard
 
-}
+} // end namespace cortex
 #endif //I960SXCHIPSET_IODEVICE_H
